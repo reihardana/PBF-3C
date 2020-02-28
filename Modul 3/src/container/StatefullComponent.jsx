95f68456d21502be552c3dae99da92ca166f5681
reihardana <reinaldohardana@gmail.com>
2020-02-28 18:15:44 +0700

    Tugas Modul 3
    Reinaldo Hardana
    1741720009

---
+++ Modul 3/src/container/StatefullComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+class StatefullComponent extends React.Component{
+    render(){
+        return <p>ini adalah statefull component</p>
+    }
+}
+export default StatefullComponent;
